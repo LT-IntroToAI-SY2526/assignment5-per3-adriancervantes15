de71042b136af5a0708c4b2fb4d9d83a5b57e445
Adrian Cervantes Lopez <acervantes-2@cps.edu>
2025-11-10 10:36:08 -0600

    secondchange

--- a5.py
+++ a5.py
@@ -163,7 +163,18 @@ class Board:
             column - index of the column to assign
             assignment - value to place at given row, column coordinate
         """
-        pass
+        self.rows[row][column] = assignment
+        self.num_nums_placed += 1
+
+        for r in range(self.size):
+            remove_if_exists(self.rows[r][column], assignment)
+
+        for c in range(self.size):
+            remove_if_exists(self.rows[row][c], assignment)
+
+        subgrid_coords = self.subgrid_coordinates(row, column)
+        for (r,c) in subgrid_coords:
+            remove_if_exists(self.rows[r][c], assignment)
 
 
 def DFS(state: Board) -> Board:
